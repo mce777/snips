43b6f6f070d8225dd5566d81250ab86f0aab0b0b
Mark Eberhard <mce777@gmail.com>
2018-05-08 16:43:54 +0200

    Update anchor-tag-fun.js

--- javascript/anchor-tag-fun.js
+++ javascript/anchor-tag-fun.js
@@ -17,3 +17,12 @@ if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
       }
   }
 }
+
+// a questionable jquery solution
+$(document).ready(function () {
+  if (window.location.href.indexOf("#unsere_veranstaltungen_auf") > -1) {
+    $('html, body').animate({
+      scrollTop: $('#unsere_veranstaltungen_auf').offset().top
+    }, 'fast');
+  }
+});
