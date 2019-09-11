000d6d3cc15c026cd554768d856a395c9bb0d26d
Mark Eberhard <mce777@gmail.com>
2019-09-11 16:19:51 +0200

    Create padding_breakpoints.tsx

---
+++ dcc/padding_breakpoints.tsx
@@ -0,0 +1,22 @@
+padding={{
+  top: {
+    [Breakpoints.default]: ContainerPadding.static200,
+      [Breakpoints.b560]: ContainerPadding.static300,
+      [Breakpoints.b1600]: ContainerPadding.static350
+  },
+  bottom: {
+    [Breakpoints.default]: ContainerPadding.static200,
+      [Breakpoints.b560]: ContainerPadding.static300,
+      [Breakpoints.b1600]: ContainerPadding.static350
+  },
+  left: {
+    [Breakpoints.default]: ContainerPadding.static370,
+      [Breakpoints.b560]: ContainerPadding.static515,
+      [Breakpoints.b1600]: ContainerPadding.static530
+  },
+  right: {
+    [Breakpoints.default]: ContainerPadding.static370,
+      [Breakpoints.b560]: ContainerPadding.static515,
+      [Breakpoints.b1600]: ContainerPadding.static530
+  }
+}}
