b984746a7bb1596e6883b0d830ea3fe840cf561e
Mark Eberhard <mce777@gmail.com>
2019-09-13 14:10:56 +0200

    Update padding_breakpoints.tsx

--- dcc/padding_breakpoints.tsx
+++ dcc/padding_breakpoints.tsx
@@ -1,22 +1,26 @@
-padding={{
-  top: {
-    [Breakpoints.default]: ContainerPadding.static200,
-      [Breakpoints.b560]: ContainerPadding.static300,
-      [Breakpoints.b1600]: ContainerPadding.static350
-  },
-  bottom: {
-    [Breakpoints.default]: ContainerPadding.static200,
-      [Breakpoints.b560]: ContainerPadding.static300,
-      [Breakpoints.b1600]: ContainerPadding.static350
-  },
-  left: {
-    [Breakpoints.default]: ContainerPadding.static370,
-      [Breakpoints.b560]: ContainerPadding.static515,
-      [Breakpoints.b1600]: ContainerPadding.static530
-  },
-  right: {
-    [Breakpoints.default]: ContainerPadding.static370,
-      [Breakpoints.b560]: ContainerPadding.static515,
-      [Breakpoints.b1600]: ContainerPadding.static530
-  }
-}}
+<Container
+  padding={{
+    top: {
+      [Breakpoints.default]: ContainerPadding.static200,
+        [Breakpoints.b560]: ContainerPadding.static300,
+        [Breakpoints.b1600]: ContainerPadding.static350
+    },
+    bottom: {
+      [Breakpoints.default]: ContainerPadding.static200,
+        [Breakpoints.b560]: ContainerPadding.static300,
+        [Breakpoints.b1600]: ContainerPadding.static350
+    },
+    left: {
+      [Breakpoints.default]: ContainerPadding.static370,
+        [Breakpoints.b560]: ContainerPadding.static515,
+        [Breakpoints.b1600]: ContainerPadding.static530
+    },
+    right: {
+      [Breakpoints.default]: ContainerPadding.static370,
+        [Breakpoints.b560]: ContainerPadding.static515,
+        [Breakpoints.b1600]: ContainerPadding.static530
+    }
+  }}
+>
+  {contents}
+</Container>
