542c5c76d3b5bdca3a643eb63ebabc48da73cacd
Mark Eberhard <mce777@gmail.com>
2019-09-13 12:51:38 +0200

    Create wrap_breakpoints.tsx

---
+++ dcc/wrap_breakpoints.tsx
@@ -0,0 +1,8 @@
+					<Container
+						wrap={{
+							[Breakpoints.default]: ContainerWrap.always,
+							[Breakpoints.b560]: ContainerWrap.never
+						}}
+					>
+						{button}
+					</Container>
