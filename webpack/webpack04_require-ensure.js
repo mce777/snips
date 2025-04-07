5631c2f00975373fadc4060b3a1247c399d94538
Mark Eberhard <mce777@gmail.com>
2025-04-07 12:08:12 +0200

    Delete webpack directory

--- webpack/webpack04_require-ensure.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-
-// This is a split point
-require.ensure([], () => {
-  // All the code in here, and everything that is imported
-  // will be in a separate file
-  const library = require('some-big-library');
-  $('foo').click(() => library.doSomething());
-});
